1da5ebbdd1ffa87ee89fbd01b8c6d4d5e2da8c68
gitnotv <97412340+gitnotv@users.noreply.github.com>
2022-06-10 20:23:57 +1000

    Add files via upload

---
+++ commands/ext.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+
+
+class context():
+
+    # Needs upgrading slow atm (0.05)
+    async def wait_for_message(client : commands.Bot, time : int, author : discord.Member = None, channel : discord.TextChannel = None, guild : discord.Guild = None):
+
+        # No arguments provided
+        if author != None and channel != None and guild != None:
+            return await client.wait_for("message", check = lambda m: m.author == author and m.channel == channel and m.guild == guild, timeout=time)
+
+        # Author only match
+        elif author != None and channel == None and guild == None:
+            return await client.wait_for("message", check = lambda m: m.author == author, timeout=time)
+
+        # Author and channel
+        elif author != None and channel != None and guild == None:
+            return await client.wait_for("message", check = lambda m: m.author == author and m.channel == channel, timeout=time)
+
+        # Channel only match
+        elif author == None and channel != None and guild == None:
+            return await client.wait_for("message", check = lambda m: m.channel == channel, timeout=time)
+
+        # Channel and guild
+        elif author == None and channel != None and guild != None:
+            return await client.wait_for("message", check = lambda m: m.author == author and m.channel == channel and m.guild == guild, timeout=time)
+
+        # Guild only match
+        elif author == None and channel == None and guild != None:
+            return await client.wait_for("message", check = lambda m: m.guild == guild, timeout=time)
+
+        # Author and guild
+        elif author != None and channel == None and guild != None:
+            return await client.wait_for("message", check = lambda m: m.author == author and m.guild == guild, timeout=time)
+
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='my-discord',
+version='0.1',
+description='discord made easy?',
+url='#',
+author='max',
+install_requires=['opencv-python'],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
